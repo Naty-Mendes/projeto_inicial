9f51dbf51119c18f85072d4854bf77d6f4d8f9b5
Natália Mendes <ns.mendes2012@gmail.com>
2024-02-22 00:28:23 -0300

    Meu primeiro commit no vs code

---
+++ main.py
@@ -0,0 +1,19 @@
+print('Jornada de python')
+
+#variavéis
+idade = 33
+nome = 'Natália'
+
+#mostrar
+print(idade)
+
+#operações
+nova_idade = idade + 1
+print(nova_idade)
+
+#lista
+lista = [1,2,3,4,5]
+print(lista)
+
+#metodos
+listas_invertida = lista.reverse
